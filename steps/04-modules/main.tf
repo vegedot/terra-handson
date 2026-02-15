475d40dab01dcf8f3ed658dc85baa15326631b13
Komatsu <kojiro.komatsu@dxc.com>
2026-02-15 21:14:17 +0900

    EC2インスタンスモジュールのサブネットIDをプライベートサブネットに変更

--- steps/04-modules/main.tf
+++ steps/04-modules/main.tf
@@ -61,7 +61,7 @@ module "ec2" {
   ami_id        = var.ami_id
   instance_type = var.instance_type
   vpc_id        = module.vpc.vpc_id
-  subnet_id     = module.vpc.public_subnet_ids[0]
+  subnet_id     = module.vpc.private_subnet_ids[0] # 最初のプライベートサブネットに配置
   environment   = var.environment
   tags          = var.tags
 }
